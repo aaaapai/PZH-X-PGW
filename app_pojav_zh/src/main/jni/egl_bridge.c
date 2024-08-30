8be8cbf0c1abfca27e4193b488a44491116422e6
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-30 23:54:10 +0800

    Update egl_bridge.c

--- app_pojav_zh/src/main/jni/egl_bridge.c
+++ app_pojav_zh/src/main/jni/egl_bridge.c
@@ -351,9 +351,9 @@ Java_org_lwjgl_opengl_GL_nativeRegalMakeCurrent(JNIEnv *env, jclass clazz) {
 EXTERNAL_API JNIEXPORT jlong JNICALL
 Java_org_lwjgl_opengl_GL_getGraphicsBufferAddr(JNIEnv *env, jobject thiz) {
     if (SpareBuffer() && pojav_environ->config_renderer == RENDERER_VIRGL)
-        return &(jlong)gbuffer;
+        return & (jlong**) gbuffer;
     else if (SpareBuffer() && pojav_environ->config_renderer == RENDERER_VK_ZINK_PREF)
-        return &(jlong)mbuffer;
+        return & (jlong**) mbuffer;
 }
 
 EXTERNAL_API JNIEXPORT jintArray JNICALL
