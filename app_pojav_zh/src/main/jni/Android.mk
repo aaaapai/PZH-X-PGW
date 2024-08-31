ccb86884dc5396f9df9c4aa2f3f9c198bad10982
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-31 12:08:01 +0800

    Update Android.mk

--- app_pojav_zh/src/main/jni/Android.mk
+++ app_pojav_zh/src/main/jni/Android.mk
@@ -11,14 +11,15 @@ LOCAL_PATH := $(HERE_PATH)
 include $(CLEAR_VARS)
 LOCAL_MODULE := angle_gles2
 LOCAL_SRC_FILES := tinywrapper/angle-gles/$(TARGET_ARCH_ABI)/libGLESv2_angle.so
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := tinywrapper
 LOCAL_SHARED_LIBRARIES := angle_gles2
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 LOCAL_SRC_FILES := tinywrapper/main.c tinywrapper/string_utils.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/tinywrapper
-LOCAL_CFLAGS += -g -rdynamic
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -27,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_LDLIBS := -ldl -llog -landroid
 # -lGLESv2
 LOCAL_MODULE := pojavexec
-LOCAL_CFLAGS += -g -rdynamic
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 LOCAL_CFLAGS += -Wno-int-conversion
 # LOCAL_CFLAGS += -DDEBUG
 # -DGLES_TEST
@@ -45,7 +46,7 @@ LOCAL_SRC_FILES := \
     input_bridge_v3.c \
     jre_launcher.c \
     utils.c \
-    driver_helper.c\
+    driver_helper.c \
     driver_helper/nsbypass.c
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
@@ -59,6 +60,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := linkerhook
 LOCAL_SRC_FILES := driver_helper/hook.c
 LOCAL_LDFLAGS := -z global
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 include $(BUILD_SHARED_LIBRARY)
 #endif
 
@@ -70,6 +72,7 @@ LOCAL_MODULE := istdio
 LOCAL_SHARED_LIBRARIES := bytehook
 LOCAL_SRC_FILES := \
     stdio_is.c
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -77,6 +80,7 @@ LOCAL_MODULE := pojavexec_awt
 LOCAL_CFLAGS += -Wno-int-conversion
 LOCAL_SRC_FILES := \
     awt_bridge.c
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 include $(BUILD_SHARED_LIBRARY)
 
 # Helper to get current thread
@@ -98,4 +102,5 @@ LOCAL_MODULE := awt_xawt
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SHARED_LIBRARIES := awt_headless
 LOCAL_SRC_FILES := xawt_fake.c
+LOCAL_CFLAGS += -O3 -fPIC -flto=auto -fwhole-program-vtables -mllvm -polly
 include $(BUILD_SHARED_LIBRARY)
