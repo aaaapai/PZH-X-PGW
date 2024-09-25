139bf9e9e7f06a40ed098ffc3c8b7a08073a4b8c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-25 18:11:25 +0800

    Update renderer_config.h

--- app_pojav_zh/src/main/jni/ctxbridges/renderer_config.h
+++ app_pojav_zh/src/main/jni/ctxbridges/renderer_config.h
@@ -3,16 +3,6 @@
 // Definitions specific to the renderer
 //
 
-
-#define RENDERER_GL4ES 1
-#define RENDERER_VK_ZINK 2
-#define RENDERER_VIRGL 3
-#define RENDERER_VULKAN 4
-#define RENDERER_VK_WARLIP 5
-#define RENDERER_VK_ZINK_PREF 6
-
-
-
 #ifndef POTATOBRIDGE_H
 #define POTATOBRIDGE_H
 #include <EGL/egl.h>
