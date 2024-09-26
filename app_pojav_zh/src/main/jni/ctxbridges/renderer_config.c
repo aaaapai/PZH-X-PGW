fe08dabe0f98bf074bc7bc1058669503e05a2295
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-26 23:53:55 +0800

    Create renderer_config.c

---
+++ app_pojav_zh/src/main/jni/ctxbridges/renderer_config.c
@@ -0,0 +1,17 @@
+#define RENDERER_GL4ES 1
+#define RENDERER_VK_ZINK 2
+#define RENDERER_VIRGL 3
+#define RENDERER_VULKAN 4
+#define RENDERER_VK_WARLIP 5
+#define RENDERER_VK_ZINK_PREF 6
+
+struct PotatoBridge {
+    void* eglContext;    // EGLContext
+    void* eglDisplay;    // EGLDisplay
+    void* eglSurface;    // EGLSurface
+    // void* eglSurfaceRead;
+    // void* eglSurfaceDraw;
+};
+
+struct PotatoBridge potatoBridge;
+EGLConfig config;
